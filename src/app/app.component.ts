984a1d1f03f643638d1d97acd4b2356da80af2ff
Long Van <longvan121996@gmail.com>
2025-01-11 23:16:51 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  title = 'social-connect-webapp';
 
   constructor() { }
 
