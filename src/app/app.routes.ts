82b49f745ab8a01c9e2d4836b178771ab2ae6d5b
Long Van <longvan121996@gmail.com>
2025-01-12 10:15:49 +0900

    add fb, privacy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+];
---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: false,
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
